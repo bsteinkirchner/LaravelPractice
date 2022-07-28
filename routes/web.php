49c021d668da3eeeb95b9c0ccbfe3a9c3b7fee8a
Brittany Steinkirchner <bsteinkirchner@outlook.com>
2022-07-28 12:58:20 -0500

    more post

--- routes/web.php
+++ routes/web.php
@@ -7,5 +7,6 @@ Route::get('/', function() {
 Auth::routes();
 
 Route::get('/p/create', 'App\Http\Controllers\PostsController@create');
+Route::post('/p', 'App\Http\Controllers\PostsController@store');
 
 Route::get('/profile/{user}', 'App\Http\Controllers\ProfilesController@index')->name('profile.show');
